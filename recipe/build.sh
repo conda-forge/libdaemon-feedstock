fb719aee9b90e680f1d25a048e4da4113e82faf5
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-09-13 17:11:14 -0500

    Define build explicitly

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,15 @@ set -ex
 mkdir -pv _build
 pushd _build
 
+EXTRA_FLAGS=""
+if [[ $(arch) == "aarch64" ]]; then
+	EXTRA_FLAGS="--build=aarch64-unknown-linux-gnu"
+fi
+
 # configure
 ${SRC_DIR}/configure \
 	--prefix=${PREFIX} \
+	$EXTRA_FLAGS \
 ;
 
 # build
